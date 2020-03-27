264674f9cf1b46a8c07430bf4acfa183bd4c1485
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2020-03-27 20:27:43 +0530

    adding bootstrap

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, redirect, request
 
+from sklearn.externals import joblib
+
+model = joblib.load('model.pkl')
+
 app = Flask(__name__)
 
 
@@ -11,6 +15,10 @@ def hello():
 def marks():
 	if request.method == 'POST':
 		hours = float(request.form['hours'])
+
+		marks = str(model.predict([[hours]])[0][0])
+	
+	return render_template('index.html', your_marks = marks)
 
 
 if __name__ == '__main__':
